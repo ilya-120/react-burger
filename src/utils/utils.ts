f5bcfa568f51f696c0412f99744cd1b25c816978
ilya120 <5374728@gmail.com>
2022-11-17 11:27:01 +0300

    "feat: JSvsTS cookie utils"

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,12 @@
-export function setCookie(name, value, props) {
+type TSetCookie = {
+  expires?: any;
+} & {
+  [key: string]: number | string | boolean;
+};
+
+export function setCookie(name: string,
+  value: string | number | boolean,
+  props?: TSetCookie) {
   props = props || {};
   let exp = props.expires;
   if (typeof exp == 'number' && exp) {
@@ -21,13 +29,13 @@ export function setCookie(name, value, props) {
   document.cookie = updatedCookie;
 }
 
-export function getCookie(name) {
+export function getCookie(name: string): string | undefined {
   const matches = document.cookie.match(
     new RegExp('(?:^|; )' + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + '=([^;]*)')
   );
   return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 
-export function deleteCookie(name) {
-  setCookie(name, null, { expires: -1 });
+export function deleteCookie(name: string) {
+  setCookie(name, false, { expires: -1 });
 }
